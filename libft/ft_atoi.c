87fc5c62c2554740c17c612008b01868d2a68917
Cemre Gozde Uyar <cuyar@k1m07s02.42istanbul.com.tr>
2023-12-09 19:09:08 +0300

    libft

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,5 +1,36 @@
 #include "libft.h"
+
 int	ft_atoi(const char *str)
 {
+	int	i;
+	int	sgn;
+	int	res;
+
+	i = 0;
+	sgn = 1;
 	
+	if (!str)
+	{
+		return (0);
+	}
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' 
+		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+	{
+		i++;
+	}
+	if (str[i] == '+')
+	{
+		i++;
+	}
+	else if (str[i] == '-')
+	{
+		sgn = -1;
+		i++;
+	}
+	res = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (res * 10) + (str[i++] - '0');
+	}
+	return (res * sgn);
 }
